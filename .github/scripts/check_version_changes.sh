49e13bc5441481fb8752c07c72ea37f89b3bb69d
Jose Saarimaa <jose.saarimaa@stat.fi>
2026-03-16 14:33:21 +0200

    Add version check script and CI job for PxApi changes
    Introduced .github/scripts/check_version_changes.sh to enforce version bump in PxApi/PxApi.csproj when PxApi is modified. Integrated this check as a dedicated job in ci.yml for PRs targeting dev (excluding dependabot). Refactored sonar pipeline to no longer trigger multiple scans.

---
+++ .github/scripts/check_version_changes.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+versionNumber=$(grep '<VersionPrefix>' ./PxApi/PxApi.csproj | grep -o "[0-9]*\.[0-9]*\.[0-9]*")
+
+git fetch origin dev --quiet
+
+versionInDev=$(git show origin/dev:PxApi/PxApi.csproj | grep '<VersionPrefix>' | grep -o "[0-9]*\.[0-9]*\.[0-9]*")
+
+echo "Version: This branch $versionNumber, dev branch $versionInDev"
+
+smallerOrEqual() {
+	if [[ $1 == $2 ]]
+	then
+		return 0
+	fi
+
+	local oldIFS=$IFS
+	IFS='.'
+	read -ra in1Vers <<< "$1"
+	read -ra in2Vers <<< "$2"
+	IFS=$oldIFS
+
+	for i in 0 1 2
+	do
+		if ((10#${in1Vers[i]} > 10#${in2Vers[i]}))
+		then
+			return 1
+		fi
+	done
+	return 0
+}
+
+if ! git diff --quiet origin/dev HEAD PxApi; then
+	if smallerOrEqual $versionNumber $versionInDev
+	then
+		echo "Version number needs to be updated."
+		exit 1
+	fi
+fi
