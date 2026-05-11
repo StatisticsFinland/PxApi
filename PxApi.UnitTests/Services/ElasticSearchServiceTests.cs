4519d7eec3ca06a182933a889266e2468fb0c6a1
LauraRocks <laura.kari@stat.fi>
2026-05-11 14:00:13 +0300

    [Feature] Add query fields support for Elasticsearch service and update search logic (#77)
    * [Feature] Add query fields support for Elasticsearch service and update search logic
    
    * [Feature] Refactor ElasticSearchService to use BoostedField records for query fields and update related tests
    
    * Update ElasticSearchServiceTests to use AllFieldGroupSample for assertions and improve code coverage justification
    
    * [Version] Update project version to 0.5.5

--- PxApi.UnitTests/Services/ElasticSearchServiceTests.cs
+++ PxApi.UnitTests/Services/ElasticSearchServiceTests.cs
@@ -7,6 +7,68 @@ namespace PxApi.UnitTests.Services
     [TestFixture]
     public class ElasticSearchServiceTests
     {
+        private static readonly string[] AllFieldGroupSample = ["title", "classificatory_variable_names", "classificatory_variable_values", "geo_variable_values"];
+
+        [TestCase(SearchTarget.Content, new[] { "title", "source", "note", "content_variable", "used_for" })]
+        [TestCase(SearchTarget.Dimension, new[] { "classificatory_variable_names" })]
+        [TestCase(SearchTarget.Value, new[] { "classificatory_variable_values" })]
+        [TestCase(SearchTarget.Geo, new[] { "geo_variable_values" })]
+        public void GetFields_KnownTarget_ReturnsExpectedFields(SearchTarget target, string[] expected)
+        {
+            string[] result = ElasticSearchService.GetFields(target);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetFields_All_ContainsAllFieldGroups()
+        {
+            string[] result = ElasticSearchService.GetFields(SearchTarget.All);
+            Assert.That(result, Is.SupersetOf(AllFieldGroupSample));
+        }
+
+        [Test]
+        public void GetFields_UnknownTarget_ReturnsContentFields()
+        {
+            string[] result = ElasticSearchService.GetFields((SearchTarget)99);
+            Assert.That(result, Is.EqualTo(ElasticSearchService.GetFields(SearchTarget.Content)));
+        }
+
+        [TestCase(SearchTarget.Content)]
+        [TestCase(SearchTarget.Dimension)]
+        [TestCase(SearchTarget.Value)]
+        [TestCase(SearchTarget.Geo)]
+        public void GetQueryFields_KnownTarget_FieldNamesMatchGetFields(SearchTarget target)
+        {
+            string[] queryFields = ElasticSearchService.GetQueryFields(target);
+            string[] expectedNames = ElasticSearchService.GetFields(target);
+            string[] actualNames = queryFields.Select(f => f.Split('^')[0]).ToArray();
+            Assert.That(actualNames, Is.EqualTo(expectedNames));
+        }
+
+        [Test]
+        public void GetQueryFields_All_FieldNamesMatchGetFieldsAll()
+        {
+            string[] queryFields = ElasticSearchService.GetQueryFields(SearchTarget.All);
+            string[] expectedNames = ElasticSearchService.GetFields(SearchTarget.All);
+            string[] actualNames = queryFields.Select(f => f.Split('^')[0]).ToArray();
+            Assert.That(actualNames, Is.EqualTo(expectedNames));
+        }
+
+        [Test]
+        public void GetQueryFields_Content_ContainsBoostedFields()
+        {
+            string[] queryFields = ElasticSearchService.GetQueryFields(SearchTarget.Content);
+            string[] boostedFields = queryFields.Where(f => f.Contains('^')).ToArray();
+            Assert.That(boostedFields, Is.Not.Empty);
+        }
+
+        [Test]
+        public void GetQueryFields_UnknownTarget_ReturnsContentQueryFields()
+        {
+            string[] result = ElasticSearchService.GetQueryFields((SearchTarget)99);
+            Assert.That(result, Is.EqualTo(ElasticSearchService.GetQueryFields(SearchTarget.Content)));
+        }
+
         [Test]
         public void MapHits_BlankId_SkipsHit()
         {
